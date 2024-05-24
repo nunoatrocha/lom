9fd367c344dab42f6b0544298ee3298838527371
Nuno Rocha <nuno.teles.rocha@gmail.com>
2024-05-24 02:40:20 +0100

    novas aulas add

---
+++ aula13/ex/exercicio13.js
@@ -0,0 +1,11 @@
+const nome = prompt("Digite o seu primeiro e último nome: ")
+
+document.body.innerHTML += `O seu no nome é: <strong>${nome}</strong><br>`
+document.body.innerHTML += `O seu nome tem: <strong>${nome.length}</strong> letras<br>`
+document.body.innerHTML += `A segunda letra do seu nome é: <strong>${nome[1]}</strong><br>`
+document.body.innerHTML += `Qual o primeiro índice da letra "o" no seu nome: <strong>${nome.indexOf("o")}</strong><br>`
+document.body.innerHTML += `Qual o último índice da letra "o" no seu nome: <strong>${nome.lastIndexOf("o")}</strong><br>`
+document.body.innerHTML += `As última 3 letras do seu nome são: <strong>${nome.slice(-3)}</strong><br>`
+document.body.innerHTML += `As palavra do seu nome são: <strong>${nome.split(" ")}</strong><br>`
+document.body.innerHTML += `O seu nome em maiúsculas é: <strong>${nome.toUpperCase()}</strong><br>`
+document.body.innerHTML += `O seu nome em minúsculas é: <strong>${nome.toLowerCase()}</strong><br>`
--- aula13/index.js
+++ aula13/index.js
@@ -1,11 +1,39 @@
 //               01234567 
-let umaString = "O rato roe"
+let umaString = "Um Texto"
 
 console.log(umaString[0])  // U
 console.log(umaString[1])  // m
 console.log(umaString[2])  // 
-console.log(umaString[3])  // t
+console.log(umaString[3])  // T
 console.log(umaString.charAt(6)) // t
-console.log(umaString.indexOf("texto")) // 3
-console.log(umaString.lastIndexOf("o", 3)) // 3
-console.log(umaString.length)
+console.log(umaString.indexOf("Texto")) // 3
+console.log(umaString.lastIndexOf("o", 3)) // -1
+console.log(umaString.length)  // 8
+
+console.log(umaString.replace(/Um/, "Outra"))  //  Outra Texto
+
+
+let novaString = "O rato roeu a roupa do rei de roma."
+
+console.log(novaString.replace(/r/g, "#"))  //  O #ato #oeu a #oupa do #ei de #oma.
+
+
+console.log(novaString.slice(2, 6))  // rato
+console.log(novaString.slice(-3))  // ma.
+console.log(novaString.slice(32))  // ma.
+
+
+console.log(novaString.split(" "))  /*  
+[
+    'O',     'rato',
+    'roeu',  'a',
+    'roupa', 'do',
+    'rei',   'de',
+    'roma.'
+]
+                                    */
+
+
+console.log(novaString.split(" ", 3))   //  [ 'O', 'rato', 'roeu' ]
+
+console.log(novaString.toUpperCase())   //   O RATO ROEU A ROUPA DO REI DE ROMA.
